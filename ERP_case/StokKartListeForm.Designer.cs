029d9735735624185ad3b42576ff4c7ceaaa8563
Dbatu00 <denizbatu@sabanciuniv.edu>
2024-01-19 13:06:04 +0300

    stok kart kayıt giriş

--- ERP_case/StokKartListeForm.Designer.cs
+++ ERP_case/StokKartListeForm.Designer.cs
@@ -30,18 +30,18 @@
         {
             this.components = new System.ComponentModel.Container();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtStokKodu = new System.Windows.Forms.TextBox();
+            this.txtStokAdı = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numBirimFiyat = new System.Windows.Forms.NumericUpDown();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBirimFiyat)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -52,41 +52,83 @@
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
-            // textBox1
+            // txtStokKodu
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtStokKodu.Location = new System.Drawing.Point(15, 41);
+            this.txtStokKodu.Name = "txtStokKodu";
+            this.txtStokKodu.Size = new System.Drawing.Size(198, 22);
+            this.txtStokKodu.TabIndex = 1;
+            this.txtStokKodu.TextChanged += new System.EventHandler(this.txtStokKodu_TextChanged);
             // 
-            // textBox2
+            // txtStokAdı
             // 
-            this.textBox2.Location = new System.Drawing.Point(16, 96);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(198, 22);
-            this.textBox2.TabIndex = 2;
+            this.txtStokAdı.Location = new System.Drawing.Point(16, 96);
+            this.txtStokAdı.Name = "txtStokAdı";
+            this.txtStokAdı.Size = new System.Drawing.Size(198, 22);
+            this.txtStokAdı.TabIndex = 2;
+            this.txtStokAdı.TextChanged += new System.EventHandler(this.txtStokAdı_TextChanged);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnKaydet);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.numericUpDown1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.numBirimFiyat);
+            this.panel1.Controls.Add(this.txtStokKodu);
+            this.panel1.Controls.Add(this.txtStokAdı);
             this.panel1.Location = new System.Drawing.Point(232, 44);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(228, 271);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // numericUpDown1
+            // btnKaydet
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(16, 154);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(198, 22);
-            this.numericUpDown1.TabIndex = 3;
+            this.btnKaydet.Location = new System.Drawing.Point(16, 200);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(198, 58);
+            this.btnKaydet.TabIndex = 7;
+            this.btnKaydet.Text = "Bilgileri Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birim Fiyat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Stok Adı";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Stok Kodu";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numBirimFiyat
+            // 
+            this.numBirimFiyat.Location = new System.Drawing.Point(16, 154);
+            this.numBirimFiyat.Name = "numBirimFiyat";
+            this.numBirimFiyat.Size = new System.Drawing.Size(198, 22);
+            this.numBirimFiyat.TabIndex = 3;
+            this.numBirimFiyat.ValueChanged += new System.EventHandler(this.numBirimFiyat_ValueChanged);
             // 
             // contextMenuStrip1
             // 
@@ -104,44 +146,6 @@
             this.label1.Text = "Stok Kart Bilgileri";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 16);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Stok Kodu";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 16);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Stok Adı";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 135);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 16);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Birim Fiyat";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 200);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 58);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Bilgileri Kaydet";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // StokKartListeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -155,7 +159,7 @@
             this.Load += new System.EventHandler(this.StokKartListe_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBirimFiyat)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,15 +168,15 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtStokKodu;
+        private System.Windows.Forms.TextBox txtStokAdı;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numBirimFiyat;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnKaydet;
         private System.Windows.Forms.Label label4;
     }
 }
--- ERP_case/StokKartListeForm.cs
+++ ERP_case/StokKartListeForm.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ERP_case
@@ -38,6 +34,49 @@ namespace ERP_case
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // kaydetme butonu
+        {
+            if(!int.TryParse(txtStokKodu.Text, out _) || string.IsNullOrEmpty(txtStokAdı.Text) || numBirimFiyat.Value <= 0)
+            {
+                MessageBox.Show("Hatalı girdi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // Specify the path to the text file
+                string filePath = "StockDetails.txt";
+
+                // Create or append to the text file and write the stock details
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine($"Stok Kodu: {txtStokKodu.Text}, Stock Name: {txtStokAdı.Text}, Unit Price: {numBirimFiyat.Value:C}");
+                }
+
+               
+                MessageBox.Show("Kayıt kaydedildi!", "Başarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtStokKodu.Text = "";
+                txtStokAdı.Text = "";
+                numBirimFiyat.Value = 0;
+
+            }
+
+        }
+
+        private void txtStokKodu_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStokAdı_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numBirimFiyat_ValueChanged(object sender, EventArgs e)
         {
 
         }
